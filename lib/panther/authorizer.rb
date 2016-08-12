d1d20799c25c8b4b83eca95f94e978d6e81ddf65
Alessandro Desantis <desa.alessandro@gmail.com>
2016-08-12 23:36:28 +0200

    Namespace Authorizer and Validator correctly

--- lib/panther/authorizer.rb
+++ lib/panther/authorizer.rb
@@ -1,33 +1,35 @@
 # frozen_string_literal: true
-class Authorizer
-  class << self
-    def authorize(model:, params:, operation:)
-      assign_params(model, params)
+module Panther
+  class Authorizer
+    class << self
+      def authorize(model:, params:, operation:)
+        assign_params(model, params)
 
-      operation.policy_klass.new(
-        model: model,
-        user: params[:current_user]
-      ).public_send("#{operation.operation_name}?")
-    end
+        operation.policy_klass.new(
+          model: model,
+          user: params[:current_user]
+        ).public_send("#{operation.operation_name}?")
+      end
 
-    def authorize!(model:, params:, operation:)
-      fail(
-        Operation::PolicyError,
-        model: model,
-        user: params[:current_user],
-        action: operation.policy_klass
-      ) unless authorize(
-        model: model,
-        params: params,
-        operation: operation
-      )
-    end
+      def authorize!(model:, params:, operation:)
+        fail(
+          Operation::PolicyError,
+          model: model,
+          user: params[:current_user],
+          action: operation.policy_klass
+        ) unless authorize(
+          model: model,
+          params: params,
+          operation: operation
+        )
+      end
 
-    private
+      private
 
-    def assign_params(model, params)
-      params.each_pair do |name, value|
-        model.send("#{name}=", value) if model.respond_to?("#{name}=")
+      def assign_params(model, params)
+        params.each_pair do |name, value|
+          model.send("#{name}=", value) if model.respond_to?("#{name}=")
+        end
       end
     end
   end
--- lib/panther/validator.rb
+++ lib/panther/validator.rb
@@ -1,36 +1,38 @@
 # frozen_string_literal: true
-class Validator
-  class << self
-    def validate(model:, params:)
-      case model
-      when Contract::Base
-        validate_contract(model, params)
-      when ActiveRecord::Base
-        validate_model(model, params)
+module Panther
+  class Validator
+    class << self
+      def validate(model:, params:)
+        case model
+        when Contract::Base
+          validate_contract(model, params)
+        when ActiveRecord::Base
+          validate_model(model, params)
+        end
       end
-    end
 
-    def validate!(model:, params:)
-      fail(
-        Operation::InvalidContractError,
-        model.errors
-      ) unless validate(model: model, params: params)
-    end
+      def validate!(model:, params:)
+        fail(
+          Operation::InvalidContractError,
+          model.errors
+        ) unless validate(model: model, params: params)
+      end
 
-    private
+      private
 
-    def validate_contract(contract, params)
-      contract.validate(params)
-    end
+      def validate_contract(contract, params)
+        contract.validate(params)
+      end
 
-    def validate_model(model, params)
-      assign_params(model, params)
-      model.validate
-    end
+      def validate_model(model, params)
+        assign_params(model, params)
+        model.validate
+      end
 
-    def assign_params(model, params)
-      params.each_pair do |name, value|
-        model.send("#{name}=", value) if model.respond_to?("#{name}=")
+      def assign_params(model, params)
+        params.each_pair do |name, value|
+          model.send("#{name}=", value) if model.respond_to?("#{name}=")
+        end
       end
     end
   end
