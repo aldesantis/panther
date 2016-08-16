32540f96b62a311cc5e5e3d046d602c11dea875d
Alessandro Desantis <desa.alessandro@gmail.com>
2016-08-16 14:58:05 +0200

    Write API docs for contracts

--- lib/panther/contract/base.rb
+++ lib/panther/contract/base.rb
@@ -5,8 +5,45 @@ module Panther
   module Contract
     # Base contract
     #
+    # Contracts are used to parse and validate incoming data, usually in write (create/update)
+    # operations.
     #
+    # They are just a thin layer on top of {Reform https://github.com/apotonick/reform}.
     #
+    # @author Alessandro Desantis <desa.alessandro@gmail.com>
+    #
+    # @see https://github.com/apotonick/reform Reform
+    #
+    # @example A simple contract
+    #   module API
+    #     module V1
+    #        module User
+    #          module Contract
+    #            class Base < ::Panther::Contract::Base
+    #              property :email
+    #
+    #              validates :email, presence: true, format: { with: /@/ }
+    #              validates_uniqueness_of :email, case_sensitive: false
+    #            end
+    #          end
+    #        end
+    #      end
+    #   end
+    #
+    # @example Contracts can inherit from other contracts
+    #   module API
+    #     module V1
+    #       module User
+    #         module Contract
+    #           class Create < Base
+    #             property :password
+    #
+    #             validates :password, presence: true, length: { in: 8..128 }
+    #           end
+    #         end
+    #       end
+    #     end
+    #   end
     class Base < Reform::Form
       include Coercion
     end
--- lib/panther/contract/types.rb
+++ lib/panther/contract/types.rb
@@ -1,6 +1,27 @@
 # frozen_string_literal: true
 module Panther
   module Contract
+    # Coercion types
+    #
+    # This module can be used to add coercion support to a contract through the awesome
+    # {Dry::Types http://dry-rb.org/gems/dry-types} gem.
+    #
+    # @author Alessandro Desantis <desa.alessandro@gmail.com>
+    #
+    # @see http://dry-rb.org/gems/dry-types Dry::Types
+    #
+    # @example A contract with coercion
+    #   module API
+    #     module V1
+    #        module User
+    #          module Contract
+    #            class Base < ::Panther::Contract::Base
+    #              property :email, type: Types::Coercible::String
+    #            end
+    #          end
+    #        end
+    #      end
+    #    end
     module Types
       include Dry::Types.module
     end
