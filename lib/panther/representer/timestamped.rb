18ab35baa4eb1fd2b3de88454df0a7c694e46e48
Alessandro Desantis <desa.alessandro@gmail.com>
2016-08-17 13:03:50 +0200

    Use class_eval with block in Representer::Timestamped

--- lib/panther/representer/timestamped.rb
+++ lib/panther/representer/timestamped.rb
@@ -3,18 +3,18 @@ module Panther
   module Representer
     module Timestamped
       def self.included(klass)
-        klass.class_eval <<-RUBY
+        klass.class_eval do
           property :created_at, exec_context: :decorator, if: -> (_) { respond_to?(:created_at) }
           property :updated_at, exec_context: :decorator, if: -> (_) { respond_to?(:updated_at) }
+        end
+      end
 
-          def created_at
-            represented.created_at.to_i
-          end
+      def created_at
+        represented.created_at.to_i
+      end
 
-          def updated_at
-            represented.updated_at.to_i
-          end
-        RUBY
+      def updated_at
+        represented.updated_at.to_i
       end
     end
   end
