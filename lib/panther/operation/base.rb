f869bc007fff1623daa0442746194c2186cdf988
Alessandro Desantis <desa.alessandro@gmail.com>
2016-08-22 18:10:02 +0200

    Document operation classes API

--- lib/panther/operation/base.rb
+++ lib/panther/operation/base.rb
@@ -1,6 +1,25 @@
 # frozen_string_literal: true
 module Panther
   module Operation
+    # An operation executes a specific task on a resource.
+    #
+    # Operations are interactors and inherit their behavior: they are stateless for the user, but
+    # they have a context which can succeed or fail.
+    #
+    # Operations always expose a +status+ attribute in their output context, which is a symbol
+    # representing the HTTP status code to respond with. They may also optionally expose a +resource+ attribute,
+    # which is an object responding to +#as_json+ to be rendered.
+    #
+    # Operations also support before, around and after callbacks.
+    #
+    # @author Alessandro Desantis <desa.alessandro@gmail.com>
+    #
+    # @abstract Subclass and override {#call} to implement an operation
+    #
+    # @see https://github.com/collectiveidea/interactor Interactor
+    #
+    # @example An example operation
+    #
     class Base
       include Interactor
 
@@ -14,16 +33,27 @@ module Panther
         end
       end
 
+      # Executes the operation
+      #
+      # @see https://github.com/collectiveidea/interactor Interactor
       def call
         fail NotImplementedError
       end
 
       protected
 
+      # Returns the operation's params
+      #
+      # This is just a shortcut for retrieving +context.params+.
+      #
+      # @return [Hash]
       def params
         context.params
       end
 
+      # Writes the parameters to the given resources, then authorizes it and validates it.
+      #
+      # @param [ActiveRecord::Base|Contract::Base] The resource to authorize and validate
       def authorize_and_validate(resource)
         write_params_to resource
 
@@ -31,15 +61,37 @@ module Panther
         validate resource
       end
 
+      # Responds with the given status code and resource.
+      #
+      # This is just a shortcut for setting +context.status+ and +context.resource+.
+      #
+      # @param status [Symbol] The status to set
+      # @param resource [ActiveRecord::Base|Representer::Base] The resource to set
       def respond_with(status: nil, resource: nil)
         context.status = status
         context.resource = resource
       end
 
+      # Responds with the given status code.
+      #
+      # This is just a shortcut for setting +context.status+. Under the hood, it calls
+      # {#respond_with} with the given status code and no resource.
+      #
+      # @param status [Symbol] The status to set
       def head(status)
         respond_with status: status
       end
 
+      # Raises the given Panther error. Passes any other arguments to the error.
+      #
+      # @param error [Symbol] The error to raise
+      #
+      # @raise [RuntimeError] If the given error is not a valid Panther error
+      # @raise [Panther::Operation::Errors::Base] The provided Panther error
+      #
+      # @example Raising an authorization error
+      #   # Raises Panther::Operation::Errors::Unauthorized with the provided arguments
+      #   fail! :unauthorized, resource: resource, user: current_user, action: :create
       def fail!(error, *args)
         klass_name = "::Panther::Operation::Errors::#{error.to_s.camelize}"
         fail "#{error} is not a defined error" unless defined?(klass_name.constantize)
--- lib/panther/operation/mixins/hooks.rb
+++ lib/panther/operation/mixins/hooks.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 module Panther
   module Operation
+    # These hooks provide some useful behavior to be shared by all operations.
+    #
+    # @author Alessandro Desantis <desa.alessandro@gmail.com>
     module Hooks
       def self.included(klass)
         klass.class_eval do
@@ -10,16 +13,27 @@ module Panther
         end
       end
 
+      # After hook: Ensures that a status code of +:ok+: is set if no status has been set for the
+      # operation.
       def ensure_status
         context.status ||= :ok
       end
 
+      # Around hook: Converts any Rails-specific errors raised in the operation into native Panther
+      # errors.
+      #
+      # For instance, converts +ActiveRecord::RecordNotFound+ into
+      # {Panther::Operation::Errors::NotFound}.
+      #
+      # @see Base#fail!
       def convert_errors(interactor)
         interactor.call
       rescue ActiveRecord::RecordNotFound
         fail! :not_found
       end
 
+      # Around hook: Recovers from Panther errors by failing the operation's context with the
+      # appropriate status code and the error as the resource.
       def handle_errors(interactor)
         interactor.call
       rescue Errors::Base => e
